564dc94f2694301b28f910fa0a58a725db40fb9a
StarT_Teg <romik.13@Mail.ru>
2020-11-29 18:24:01 +0300

    ДЗ по 4 уроку

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,9 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 public class Main extends Application {

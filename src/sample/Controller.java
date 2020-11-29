b8a4c3b1d59ccd7acca8a0fc29412f2b36e3eaf8
StarT_Teg <romik.13@Mail.ru>
2020-11-29 17:54:43 +0300

    ДЗ по 4 уроку

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,57 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Popup;
+import javafx.stage.Stage;
+
 public class Controller {
+
+    @FXML
+    private TextField inputField;
+
+    @FXML
+    private ListView <String> listView;
+
+    ObservableList <String> messageList = FXCollections
+            .observableArrayList();
+
+    @FXML
+    public void initialize (){
+
+        // FXML не работает с обычными массивами
+        // Для добавления массива необходимо использовать оболочку ObservableList
+
+        listView.setItems(messageList);
+
+    }
+
+    @FXML
+    public void addWordToList (){
+        // По умолчанию мы не можем просто так добавить элемент в спписок
+        // До этого необходимо получить список всех элементов
+
+        String word = inputField.getText();
+
+        if (word.length() != 0){
+            listView.getItems().add(word);
+        }
+
+        inputField.setText("");
+
+    }
+
+    @FXML
+    public void exit (){
+        System.exit(0);
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,13 +16,16 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource(PATH_SAMPLE_XML));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Сетевой чат (В ПРОЦЕССЕ РАЗРАБОТКИ)");
         primaryStage.setScene(new Scene(root));
         primaryStage.show(); // Видимость
     }
 
 
+
     public static void main(String[] args) {
         launch(args);
     }
+
+
 }
